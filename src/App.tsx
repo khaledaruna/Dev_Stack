3a9daf426fff88e447629b457bc9dd721d411749
khaledaruna <khaledajaman@gmail.com>
2026-09-13 19:53:46 +0600

    Initialize React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// const App = () => {
+//   return (
+//     <>
+    
+//       <Navbar />
+//       <Hero />
+//       <Technologies technologiesPromise={technologiesPromise} />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default App;
+
+function App() {
+  return (
+    <div>
+      <h1>Dev Stack</h1>
+    </div>
+  );
+}
+
+export default App;
