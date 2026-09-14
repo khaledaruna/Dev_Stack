1485a7d7d6c2a3a4efb0b3f0c48b87f70b7b42be
khaledaruna <khaledajaman@gmail.com>
2026-09-14 20:35:16 +0600

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: 'Dev_Stack',
+  base: "/Dev_Stack/",
   plugins: [react(), tailwindcss(),],
-})
+});
