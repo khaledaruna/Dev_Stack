f0e416800acfd7bdab475ecaea70d7bcd47e108e
khaledaruna <khaledajaman@gmail.com>
2026-09-14 17:17:35 +0600

    Update README.me preview image link and other functionalities

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Dev_Stack/',
+  // base: '/Dev_Stack/',
   plugins: [react(), tailwindcss(),],
 })
